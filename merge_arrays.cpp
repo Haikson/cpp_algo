9339aae5f82e537e78364fd9c810d0f644b7bdbd
Kamo Petrosyan <kamo@haikson.com>
2018-03-31 22:48:58 +0300

    modified:   merge_arrays.cpp

--- merge_arrays.cpp
+++ merge_arrays.cpp
@@ -4,43 +4,45 @@ using namespace std;
 
 #define count_of(x) (sizeof(x)/sizeof(*x))
 
-int * merge_arrays(int * array1, int * array2){
-//    int size_of_1 = sizeof(array1) / sizeof(array1[0]);
-    int size_of_1 = count_of(array1);
-//    int size_of_2 = sizeof(array2) / sizeof(array2[0]);
-    int size_of_2 = count_of(array2);
-
-    int size_of_arrays = size_of_1 + size_of_2;
-    int * merged_array = new int[size_of_arrays];
-    for (int i = 0; i <= size_of_1; ++i) {
-        merged_array[i] = array1[i];
-    }
-    int start = 0;
-    for (int i = 0; i <= size_of_2; ++i) {
-        for (int j = start; j < size_of_arrays; ++j) {
-            if (array2[i] <= merged_array[j] || merged_array[j] > merged_array[j+1]){
-                int tmp = merged_array[j+1];
-                for (int a = size_of_arrays; a > j - 1; --a){
-                    merged_array[a] = merged_array[a-1];
-                }
-                merged_array[j] = array2[i];
-                start = j;
-                break;
-            }
+void merge(int m, int n, int A[], int B[], int C[]) {
+    int i, j, k;
+    i = 0;
+    j = 0;
+    k = 0;
+    while (i < m && j < n) {
+        if (A[i] <= B[j]) {
+            C[k] = A[i];
+            i++;
+        } else {
+            C[k] = B[j];
+            j++;
         }
-
+        k++;
     }
 
-    cout << endl;
-    return merged_array;
+    if (i < m) {
+        for (int p = i; p < m; p++) {
+            C[k] = A[p];
+            k++;
+        }
+    } else {
+        for (int p = j; p < n; p++) {
+            C[k] = B[p];
+            k++;
+        }
+    }
 }
 
 int main() {
-    int arr1[3] = {1,2,3};
-    int arr2[3] = {5,6,7};
-    int * merged_array = merge_arrays(arr1, arr2);
+    int arr2[3] = {1,2,3};
+    int arr1[3] = {5,6,7};
 
-    for (int i = 0; i < count_of(arr1) + count_of(arr2); ++i) {
+    int m = count_of(arr1);
+    int n = count_of(arr2);
+    int * merged_array = new int[m+n+5];
+    merge(m, n, arr1, arr2, merged_array);
+
+    for (int i = 0; i < m + n + 5; ++i) {
         cout << merged_array[i] << " ";
     }
     cout << endl;
